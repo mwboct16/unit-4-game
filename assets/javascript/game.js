7d3006ef5ba9d199287ff55729eb7593a11e854c
Michael Black <mwboct16@gmail.com>
2019-02-16 20:01:51 -0500

    Initial Push

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+/* Steps to be taken
+- Create function that will Generate random number
+- display random number generated to the `random-number` div
+- create function that will generate a random number for each picture
+- when user clicks the picture, display the value
+- whenever the value is displayed, add it to last value
+- if value of user > the value provided by the console, the user loses
+- reset javascript without having to reload the page
+*/
+
+$('document').ready()
+
+function pcRandomNum(random) {
+}
