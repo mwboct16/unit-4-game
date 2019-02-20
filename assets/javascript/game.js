2dd9dad8c3c8a6d7517f29b07c1012f8ca7dc652
Michael Black <mwboct16@gmail.com>
2019-02-19 22:01:17 -0500

    Finally!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,24 +10,24 @@
 
 $('document').ready(function () {
 
-var computerDecision = null
-var wins = 0;
-var losses = 0;
-var playerTotal = 0;
+    var computerDecision = null
+    var wins = 0;
+    var losses = 0;
+    var playerTotal = 0;
 
-var computerChoice = function () {
-    computerDecision = Math.floor(Math.random() * 40 + 10);
-    // Numbers between 10 and 40
-    return computerDecision;    
-}
+    var computerChoice = function () {
+        computerDecision = Math.floor(Math.random() * 40 + 10);
+        // Numbers between 10 and 40
+        return computerDecision;
+    }
 
-computerChoice();
-console.log(computerDecision);
- 
-// display of pc generated random number
-var computerChoiceText = document.getElementById('random-number');
-computerChoiceText.innerHTML = computerDecision.toString();
-});
+    computerChoice();
+    console.log(computerDecision);
+
+    // display of pc generated random number
+    var computerChoiceText = document.getElementById('random-number');
+    computerChoiceText.innerHTML = computerDecision.toString();
+
 
 // each picture gets random number
 var greenJewel = Math.floor(Math.random() * 10);
@@ -38,57 +38,62 @@ var tealJewel = Math.floor(Math.random() * 10);
 console.log(greenJewel);
 console.log(orangeJewel);
 console.log(purpleJewel);
-console.log(tealJewel); 
+console.log(tealJewel);
 
 // game reset
-var reset = function (){
+ var reset = function () {
     computerChoice();
     greenJewel;
     orangeJewel;
     purpleJewel;
     tealJewel;
     playerTotal = 0;
-}
+} 
 
-var rules = function(){
+
+var rules = function () {
     if (playerTotal > computerDecision) {
-    losses++
-    document.getElementById('loss').innerHTML = losses 
-    reset();
-    } else if (playerTotal === computerDecision){
-    wins++
-    reset();
+        losses++;
+        document.getElementById('losses').innerHTML = losses;
+        reset();
+    } else if (playerTotal === computerDecision) {
+        wins++;
+        document.getElementById('wins').innerHTML = losses;
+        reset();
     }
-}
+};
 
+var totalDisplay = document.getElementById('total-score-display')
 // each time the picture gets clicked, the value displays in the provided div
-$("#green").on("click", function() {
+
+$("#green").click(function () {
+    console.log("clicked");
     playerTotal = playerTotal + greenJewel;
-    document.getElementById('total-score-display').innerHTML = greenJewel;
-    rules();  
+    totalDisplay.innerHTML = playerTotal;
+    rules();
     console.log(playerTotal);
 });
 
-$("#orange").on("click", function() {
+$("#orange").on("click", function () {
     playerTotal = playerTotal + orangeJewel;
-    document.getElementById('total-score-display').innerHTML = orangeJewel;    
-    rules();  
+    totalDisplay.innerHTML = playerTotal;
+    rules();
     console.log(playerTotal);
 });
 
-$("#purple").on("click", function() {
+$("#purple").on("click", function () {
     playerTotal = playerTotal + purpleJewel;
-    document.getElementById('total-score-display').innerHTML = purpleJewel;    
-    rules();  
+    totalDisplay.innerHTML = playerTotal;
+    rules();
     console.log(playerTotal);
 });
-$("#teal").on("click", function() {
+$("#teal").on("click", function () {
     playerTotal = playerTotal + tealJewel;
-    document.getElementById('total-score-display').innerHTML = tealJewel;    
-    rules();  
+    totalDisplay.innerHTML = playerTotal;
+    rules();
     console.log(playerTotal);
 });
-
+});
 
 
 
